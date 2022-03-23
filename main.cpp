4d0c00d27c0b8b565aea444756a81383d4b4cd81
Jatin Dalal <jd.jatindalal@gmail.com>
2022-03-23 15:42:55 +0530

    Remove Dark Style that was causing lag
    The commits that contain dark style will experience delay once after interacting with editor

--- main.cpp
+++ main.cpp
@@ -54,17 +54,6 @@ int main(int argc, char *argv[])
     qInstallMessageHandler(customMessageHandler);
     QApplication a(argc, argv);
 
-    QFile f(":qdarkstyle/dark/style.qss");
-
-    if (!f.exists())   {
-        printf("Unable to set stylesheet, file not found\n");
-    }
-    else   {
-        f.open(QFile::ReadOnly | QFile::Text);
-        QTextStream ts(&f);
-        qApp->setStyleSheet(ts.readAll());
-    }
-
     Tool w;
     w.show();
 
